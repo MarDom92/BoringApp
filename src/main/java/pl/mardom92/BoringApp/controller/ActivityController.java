2a22c7b1ac7631bc651982b84d5159c89386da56
MarDom92 <marcin.domanski92@gmail.com>
2022-04-29 18:06:38 +0200

    add controller and service of activity

---
+++ src/main/java/pl/mardom92/BoringApp/controller/ActivityController.java
@@ -0,0 +1,7 @@
+package pl.mardom92.BoringApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActivityController {
+}
---
+++ src/main/java/pl/mardom92/BoringApp/service/ActivityService.java
@@ -0,0 +1,9 @@
+package pl.mardom92.BoringApp.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequestMapping(path = "/")
+public class ActivityService {
+}
