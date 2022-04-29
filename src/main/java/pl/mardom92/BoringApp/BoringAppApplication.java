7c976e7658ced8bdce6026842940e17a69fe70be
MarDom92 <marcin.domanski92@gmail.com>
2022-04-29 17:42:02 +0200

    init commit

---
+++ src/main/java/pl/mardom92/BoringApp/BoringAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mardom92.BoringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mardom92/BoringApp/BoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mardom92.BoringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
