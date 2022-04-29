7454ae356b44a86edb76d294fdfe76d3de4778cc
MarDom92 <marcin.domanski92@gmail.com>
2022-04-29 18:59:16 +0200

    add repository of activity

---
+++ src/main/java/pl/mardom92/BoringApp/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package pl.mardom92.BoringApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.mardom92.BoringApp.model.Activity;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+}
